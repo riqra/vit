0926934ff24269038d615508463899286220960a
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-08-24 10:39:44 -0500

    add comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import reportWebVitals from './reportWebVitals';
 import vit from "./vit";
 
 window.__VIT__ = vit;
-window.vit = vit; //this is supported until 
+window.vit = vit; //this is supported until September 30th 2022
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
