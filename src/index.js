74900eb44de0fdd78ae8e049b0ff88597a8559a7
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-08-24 10:33:20 -0500

    give support to window.vit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import reportWebVitals from './reportWebVitals';
 import vit from "./vit";
 
 window.__VIT__ = vit;
+window.vit = vit; //this is supported until 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
