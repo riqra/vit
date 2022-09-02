6774fd1ef4061d7c363257e37ac7a3299bafc6cb
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-01 21:45:53 -0500

    create typography

---
+++ src/globals/style/typography.ts
@@ -0,0 +1,14 @@
+const fontFamily = "'Open Sans', sans-serif";
+
+const fontSize = {
+  s12: '12px',
+  s14: '14px',
+  s16: '16px'
+}
+
+const typography = {
+  fontFamily,
+  fontSize
+}
+
+export default typography;
