feb702fa2dcf5efa4ac4a9eee14503e3c02b09cf
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-01 23:13:46 -0500

    add comments

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export {
   FreeShippingProgressBarInSnackbarRender as freeShippingProgressBarInSnackbar
 };
 
+// https://es.reactjs.org/docs/jsx-in-depth.html
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
