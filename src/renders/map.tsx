f203306b749f671b9b0d6888aa7876bda78c7a72
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-02 04:06:56 -0500

    create map

---
+++ src/renders/map.tsx
@@ -0,0 +1,18 @@
+import ModalForImages from "../components/custom/ModalForImages";
+import { iModalForImages } from "../components/custom/ModalForImages/contracts";
+import FreeShippingProgressBarInSnackbar from "../components/custom/ProgressBarInSnackbar";
+import { iFreeShippingProgressBarInSnackbar } from "../components/custom/ProgressBarInSnackbar/contracts";
+import WhatsappButton from "../components/custom/WhatsappButton";
+import { iWhatsappButton } from "../components/custom/WhatsappButton/contracts";
+import render from "./render";
+
+const whatsappButton = (props: iWhatsappButton) => render(props.id, <WhatsappButton {...props} />);
+const modalForImages = (props: iModalForImages) => render(props.id, <ModalForImages {...props} />);
+const freeShippingProgressBarInSnackbar = (props: iFreeShippingProgressBarInSnackbar) =>
+  render(props.id, <FreeShippingProgressBarInSnackbar {...props} />);
+
+export {
+  whatsappButton,
+  modalForImages,
+  freeShippingProgressBarInSnackbar
+}
