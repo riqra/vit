dbf68de4a48b35c9a11e98accf56d1d8037a662e
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-01 23:13:07 -0500

    create contract

---
+++ src/components/custom/ProgressBarInSnackbar/contracts.ts
@@ -0,0 +1,11 @@
+interface iFreeShippingProgressBarInSnackbar {
+  id: string,
+  cartValue: number,
+  minCartValueForFreeShipping: number,
+  currencySymbol: string,
+  autoHideDuration: number,
+  progressBarColor: string,
+  progressBarBackgroundColor: string
+}
+
+export type { iFreeShippingProgressBarInSnackbar }
