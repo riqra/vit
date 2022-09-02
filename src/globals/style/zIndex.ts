eeb1de1dd173d6cd1995409321fa4b86d9643ccd
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-01 21:46:24 -0500

    create zIndex

---
+++ src/globals/style/zIndex.ts
@@ -0,0 +1,13 @@
+const base = 1000;
+
+const zIndex = {
+  mobileStepper: base,
+  fab: base + 50,
+  appBar: base + 100,
+  drawer: base + 200,
+  modal: base + 300,
+  snackbar: base + 400,
+  tooltip: base + 500
+}
+
+export default zIndex;
