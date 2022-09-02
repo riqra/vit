c5e74e08bde55bcffd4e946697366584eb46d5f3
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-02 04:31:30 -0500

    update references

--- src/renders/map.tsx
+++ src/renders/map.tsx
@@ -1,13 +1,15 @@
 import ModalForImages from "../components/custom/ModalForImages";
 import { iModalForImages } from "../components/custom/ModalForImages/contracts";
-import FreeShippingProgressBarInSnackbar from "../components/custom/ProgressBarInSnackbar";
-import { iFreeShippingProgressBarInSnackbar } from "../components/custom/ProgressBarInSnackbar/contracts";
+import FreeShippingProgressBarInSnackbar from "../components/custom/FreeShippingProgressBarInSnackbar";
+import { iFreeShippingProgressBarInSnackbar } from "../components/custom/FreeShippingProgressBarInSnackbar/contracts";
 import WhatsappButton from "../components/custom/WhatsappButton";
 import { iWhatsappButton } from "../components/custom/WhatsappButton/contracts";
 import render from "./render";
 
 const whatsappButton = (props: iWhatsappButton) => render(props.id, <WhatsappButton {...props} />);
+
 const modalForImages = (props: iModalForImages) => render(props.id, <ModalForImages {...props} />);
+
 const freeShippingProgressBarInSnackbar = (props: iFreeShippingProgressBarInSnackbar) =>
   render(props.id, <FreeShippingProgressBarInSnackbar {...props} />);
 
