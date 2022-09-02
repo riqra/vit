dfaf932d2204c06bdb9a9d33197ac7e86280802a
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-02 16:20:37 -0500

    comment proposed flow

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,30 @@ import reportWebVitals from './reportWebVitals';
 reportWebVitals();
 
 export * from './renders/map';
+
+/*
+<!-- UPDATE CART -->
+
+<script>
+  var lastCartValue = {{LastCartValue}} (200)
+  var minCartValueForFreeShipping = {{minCartValueForFreeShipping}} (100)
+  var cartValue = {{CartValue}} (150)
+  // DEFINE A VARIABLE IN GTM
+  datalayer({
+    lastCartValue: updateCart.cartValue
+  })
+  
+  if (cartValue <= minCartValueForFreeShipping) {
+    // render snackbar
+  } else if (cartValue < lastValue) {
+    // render snackbar
+  }
+  
+  qué pasa si aún no llego al mínimo
+  qué pasa si acabo de llegar al mínimo
+  qué pasa si hago operaciones por encima del mínimo sin haber bajado del mínimo
+  
+  agregar parámetro para mostrar desde cierto monto
+  
+</script>
+*/
