52ce65760b8ef63353c969b2eff69d4994a9e641
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-02 04:07:10 -0500

    use map

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import reportWebVitals from './reportWebVitals';
-import WhatsappButtonRender from './renders/WhatsappButtonRender';
-import ModalForImagesRender from './renders/ModalForImagesRender';
-import FreeShippingProgressBarInSnackbarRender from './renders/FreeShippingProgressBarInSnackbarRender';
-
-export {
-  WhatsappButtonRender as whatsappButton,
-  ModalForImagesRender as modalForImages,
-  FreeShippingProgressBarInSnackbarRender as freeShippingProgressBarInSnackbar
-};
-
-// https://es.reactjs.org/docs/jsx-in-depth.html
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export * from './renders/map';
