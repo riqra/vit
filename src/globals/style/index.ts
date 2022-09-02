d6ffdeec89005af4278690048eb5eb05d593ef0c
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-01 21:44:31 -0500

    rename colors to palette, add zIndex and typography

--- src/globals/style/index.ts
+++ src/globals/style/index.ts
@@ -1,16 +1,21 @@
 import breakpoint from "./breakpoint";
-import color from "./color";
+import palette from "./palette";
 import spacing from "./spacing";
+import typography from "./typography";
+import zIndex from "./zIndex";
 
 const style = {
-  color,
+  palette,
   spacing,
-  breakpoint
+  breakpoint,
+  zIndex,
+  typography
 }
 
 // https://www.figma.com/file/aLA4vbkhSqfZlZVSAh84XR/Truck---Storefront?node-id=351%3A516
 // https://gitlab.com/gitlab-org/gitlab-ui/-/blob/main/src/scss/variables.scss
 // https://uxplanet.org/everything-you-should-know-about-8-point-grid-system-in-ux-design-b69cb945b18d
 // https://medium.com/codyhouse/create-your-design-system-part-4-spacing-895c9213e2b9
+// https://fonts.google.com/specimen/Open+Sans?query=open+sans
 
 export default style;
