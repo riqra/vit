72cda0bde963c50339c65fcbf4080a37450bbbc8
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2022-09-29 18:38:30 -0500

    refactor spacing

--- src/globals/style/spacing.ts
+++ src/globals/style/spacing.ts
@@ -1,15 +1,20 @@
 const gridSize = 8;
 
 const spacing = {
-  s2: gridSize * 0.25 + 'px',
-  s4: gridSize * 0.5 + 'px',
-  s8: gridSize + 'px',
-  s16: gridSize * 2 + 'px',
-  s24: gridSize * 3 + 'px',
-  s32: gridSize * 4 + 'px',
-  s40: gridSize * 5 + 'px',
-  s48: gridSize * 6 + 'px',
-  gridSize: gridSize
+  /**
+   * 
+   * @param factor the multiplier of the gridSize
+   * @returns the expected size in pixels
+   */
+  s: (factor: number): string => (gridSize * factor + 'px'),
+  get s2() { return this.s(0.25) },
+  get s4() { return this.s(0.5) },
+  get s8() { return this.s(1) },
+  get s16() { return this.s(2) },
+  get s24() { return this.s(3) },
+  get s32() { return this.s(4) },
+  get s40() { return this.s(5) },
+  get s48() { return this.s(6) }
 }
 
 export default spacing;
